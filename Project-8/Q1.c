fbbb79e4c6e35509c2a3ba86f60089a231ae9355
Krunalm26 <154975231+Krunalm26@users.noreply.github.com>
2025-06-20 14:20:39 +0530

    Update Q1.c

--- Project-8/Q1.c
+++ Project-8/Q1.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    char str[100];
+    char string[100];
     char *ptr;
     int length = 0;
 
-    // Input
+
     printf("Enter any string: ");
-    fgets(str, sizeof(str), stdin);
+    fgets(string, sizeof(str), stdin);
 
-    // Point to the beginning of the string
-    ptr = str;
+    
+    ptr = string;
 
-    // Loop through the string using pointer until null character
+    
     while (*ptr != '\0') {
         if (*ptr != '\n')  // To ignore newline character from fgets
             length++;
         ptr++;
     }
 
-    // Output
+
     printf("The length of a string is: %d\n", length);
 
     return 0;
